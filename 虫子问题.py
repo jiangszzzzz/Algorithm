7752cb8915e511d1d64a27bdc6099ed79b2fdaa5
jack <jiangsz92@163.com>
2022-04-12 17:07:31 +0800

    Initial commit

---
+++ 虫子问题.py
@@ -0,0 +1,13 @@
+# 幼虫 2 days
+# 成熟 一天下一个仔 3days
+# 死亡 幼虫到成年虫子 5天
+# 幼虫到死亡 五天时间 下三个仔
+
+while True:
+    try:
+        s = int(input())
+        
+    except:
+        break
+bugs = [0, 0, 1, 0, 0]
+for
