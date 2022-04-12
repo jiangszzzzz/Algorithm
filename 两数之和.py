683401e6bc1a33f40802f60b5f362f841bb0dcb3
jack <jiangsz92@163.com>
2022-04-12 23:19:55 +0800

    feat

---
+++ 两数之和.py
@@ -0,0 +1,18 @@
+# 输入 int x
+# 求数组中第一个出现的. 和为x 的 两个数
+# 要求复杂度低于 n**2
+
+# 复杂度为n的方法
+# 显存字典 然后判断
+while True:
+    try:
+        x = int(input())
+        l1 = list(map(int, input().split(' ')))
+        a = 0
+        d1 = {}
+        for i, num in enumerate(l1):
+            d1[i] = num
+            if d1.get(x - num) is not None:
+                print(num, x - num)
+    except:
+        break
